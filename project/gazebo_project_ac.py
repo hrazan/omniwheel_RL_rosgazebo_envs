0a94b3e6977fa02b7a0f76e2c8b9003b259d5612
hrazan <hafiyanda.razan@gmail.com>
2020-05-13 17:35:08 +0900

    actor_critic v3.2
    reward edit

--- project/gazebo_project_ac.py
+++ project/gazebo_project_ac.py
@@ -182,13 +182,20 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         prev_distance = self.euclidean_distance(self.beforepose, self.goalpose)
 
         if not done:
+        """
             reward = prev_distance - cur_distance
             if cur_distance < self.subgoal_as_dist_to_goal :
-                reward += self.subgoal_as_dist_to_goal - cur_distance
+                reward += (self.subgoal_as_dist_to_goal - cur_distance)*10
                 self.subgoal_as_dist_to_goal = cur_distance
         else:
             reward = -10
-    
+        """
+            if  cur_distance < prev_distance and cur_distance < self.subgoal_as_dist_to_goal :
+                reward = 100/distance
+                self.subgoal_as_dist_to_goal = cur_distance
+            else: reward = 0.1
+        else:
+            reward = 0.01
         self.beforepose.x = self.pose.x
         self.beforepose.y = self.pose.y
         
