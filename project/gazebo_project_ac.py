eb5b8a6c3069340e2c738d3c92369c563f0c4978
hrazan <hafiyanda.razan@gmail.com>
2020-05-17 18:16:19 +0900

    actor_critic v4.0
    adding degree and subgoal to state

--- project/gazebo_project_ac.py
+++ project/gazebo_project_ac.py
@@ -5,22 +5,19 @@ import time
 import numpy as np
 import random
 import tf
-
-from math import pow, atan2, sqrt, exp
+from math import pow, atan2, sqrt, exp, pi
 
 from gym import utils, spaces
 from gym_gazebo.project_envs import gazebo_env
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty
-
 from gazebo_msgs.msg import ModelState, ModelStates
 from turtlesim.msg import Pose
-
 from gazebo_msgs.srv import SetModelState
-
 from sensor_msgs.msg import LaserScan
-
 from gym.utils import seeding
+
+
 
 class ProjectAcEnv(gazebo_env.GazeboEnv):
 
@@ -40,11 +37,12 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         self.startpose = Pose()
 
         self.goal = False
+        self.update_subgoal = False
 
         self.goalpose.x = 15.000
         self.goalpose.y = 0.000
         self.get_pose(self.beforepose)
-        self.subgoal_as_dist_to_goal = 20 # max. lidar's value
+        self.subgoal_as_dist_to_goal = 30 # max. lidar's value
 
 		# Action space design
         """
@@ -63,7 +61,7 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         """
         shape(lidar sensors + distance,)
         """
-        self.observation_space = spaces.Box(low = 0, high = 20, shape=(12,), dtype=np.float32)
+        self.observation_space = spaces.Box(low = 0, high = 20, shape=(13,), dtype=np.float32)
 		
         self._seed()
 
@@ -126,8 +124,12 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
             self.pose.x = round(data.pose[12].position.x, 4)
             self.pose.y = round(data.pose[12].position.y, 4)
             quaternion = (round(data.pose[12].orientation.x, 4), round(data.pose[12].orientation.y, 4), round(data.pose[12].orientation.z, 4), round(data.pose[12].orientation.w, 4))
-            self.pose.theta = tf.transformations.euler_from_quaternion(quaternion)[2]
-            print self.pose.theta
+            yaw = round((tf.transformations.euler_from_quaternion(quaternion)[2]*180/pi), 4)
+            """yaw = [0.0001,360]"""
+            if yaw <= 0:
+                self.pose.theta = yaw + 360
+            else:
+                self.pose.theta = yaw
         except IndexError:
             None
 
@@ -135,11 +137,22 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         """Euclidean distance between current pose and the goal"""
         return round(sqrt(pow((goal_pose.x - now_pose.x), 2) + pow((goal_pose.y - now_pose.y), 2)),4)
 
+    def euclidean_degree(self, now_pose, goal_pose):
+        """degree = [0.0001,360]"""
+        degree = round((np.arcsin((goal_pose.y - now_pose.y)/self.euclidean_distance(now_pose, goal_pose))*180/pi), 4)
+        if (goal_pose.x - now_pose.x) < 0:
+            degree = 180 - degree
+        else:
+            if degree <= 0:
+                degree += 360
+        return degree
+    
     def calculate_observation(self,data):
         min_range = 0.301
         done = False
-        laser_used = self.observation_space.shape[0]-1
         state_list = []
+        """
+        laser_used = self.observation_space.shape[0]-1
         for i, item in enumerate(data.ranges):
             if (i==0) or (((i+1)%(len(data.ranges)/(laser_used-1)))==0):
                 if not np.isinf(data.ranges[i]):    
@@ -148,8 +161,30 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
                     state_list += [data.ranges[i]]
                 else:
                     state_list += [30]
+        """
+        for i, item in enumerate(data.ranges):
+            if not np.isinf(data.ranges[i]):    
+                if (min_range > data.ranges[i] > 0):
+                    done = True
+                state_list += [data.ranges[i]]
+            else:
+                state_list += [30]
+                
         distance = self.euclidean_distance(self.pose, self.goalpose)
-        state_list += [distance]
+        
+        body_to_target_degree = self.euclidean_degree(self.pose, self.goalpose)
+        if body_to_target_degree > self.pose.theta:
+            direction_to_target_deg = body_to_target_degree - self.pose.theta
+        else:
+            direction_to_target_deg = 360 - (self.pose.theta - body_to_target_degree)
+        
+        cur_distance = self.euclidean_distance(self.pose, self.goalpose)
+        if cur_distance < self.subgoal_as_dist_to_goal :
+            self.subgoal_as_dist_to_goal = cur_distance
+            self.update_subgoal = True
+        
+        state_list += [distance, direction_to_target_deg, self.subgoal_as_dist_to_goal]
+        #print body_to_target_degree, direction_to_target_deg
         state_tuple = tuple(state_list)
         return state_tuple, done
 
@@ -183,17 +218,16 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
             print ("/gazebo/pause_physics service call failed")
 
         state, done = self.calculate_observation(data)
-        avg_data = sum(state)/len(state)
-
+        
         cur_distance = self.euclidean_distance(self.pose, self.goalpose)
         prev_distance = self.euclidean_distance(self.beforepose, self.goalpose)
 
         if not done:
-            """
-            reward = prev_distance - cur_distance
-            if cur_distance < self.subgoal_as_dist_to_goal :
-                reward += (self.subgoal_as_dist_to_goal - cur_distance)*10
-                self.subgoal_as_dist_to_goal = cur_distance
+            if self.update_subgoal:
+                reward = 10
+                self.update_subgoal = False
+            else:
+                reward = (prev_distance - cur_distance)*10
         else:
             reward = -10
             """
@@ -203,6 +237,8 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
             else: reward = 0.1
         else:
             reward = 0.01
+            """
+        
         self.beforepose.x = self.pose.x
         self.beforepose.y = self.pose.y
         
@@ -248,6 +284,6 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
 
         self.beforepose.x = self.pose.x
         self.beforepose.y = self.pose.y
-        self.subgoal_as_dist_to_goal = 20
+        self.subgoal_as_dist_to_goal = self.euclidean_distance(self.pose, self.goalpose)
 
         return np.asarray(state)
