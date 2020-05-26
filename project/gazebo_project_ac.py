d34579238124df4e0a92f610c9ba66f040ac0daf
hrazan <hafiyanda.razan@gmail.com>
2020-05-26 16:37:12 +0900

    actor_critic v5.0 trying paper's movement
    just Vx and w for actions and static pillars for obstacles

--- project/gazebo_project_ac.py
+++ project/gazebo_project_ac.py
@@ -38,6 +38,7 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
 
         self.goal = False
         self.update_subgoal = False
+        self.robot_id = 12
 
         self.goalpose.x = 15.000
         self.goalpose.y = 0.000
@@ -52,16 +53,23 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
             dtype = np.float32
         )
         """
+        """
         self.action_space = spaces.Box(
             low = np.array([0.0, -0.3, -0.3]),
             high = np.array([0.3, 0.3, 0.3]),
             dtype = np.float32
         )
+        """
+        self.action_space = spaces.Box(
+            low = np.array([0.0, -0.3]),
+            high = np.array([0.3, 0.3]),
+            dtype = np.float32
+        )
         
         """
-        shape(lidar sensors + distance,)
+        shape(lidar sensors + distance + angle,)
         """
-        self.observation_space = spaces.Box(low = 0, high = 20, shape=(13,), dtype=np.float32)
+        self.observation_space = spaces.Box(low = 0, high = 20, shape=(12,), dtype=np.float32)
 		
         self._seed()
 
@@ -121,15 +129,18 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
 
     def update_pose(self, data):
         try:
-            self.pose.x = round(data.pose[12].position.x, 4)
-            self.pose.y = round(data.pose[12].position.y, 4)
-            quaternion = (round(data.pose[12].orientation.x, 4), round(data.pose[12].orientation.y, 4), round(data.pose[12].orientation.z, 4), round(data.pose[12].orientation.w, 4))
+            self.pose.x = round(data.pose[self.robot_id].position.x, 4)
+            self.pose.y = round(data.pose[self.robot_id].position.y, 4)
+            quaternion = (round(data.pose[self.robot_id].orientation.x, 4), round(data.pose[self.robot_id].orientation.y, 4), round(data.pose[self.robot_id].orientation.z, 4), round(data.pose[self.robot_id].orientation.w, 4))
+            yaw = round((tf.transformations.euler_from_quaternion(quaternion)[2]), 4)
+            """
             yaw = round((tf.transformations.euler_from_quaternion(quaternion)[2]*180/pi), 4)
-            """yaw = [0.0001,360]"""
+            #yaw = [0.0001,360]
             if yaw <= 0:
                 self.pose.theta = yaw + 360
             else:
                 self.pose.theta = yaw
+            """
         except IndexError:
             None
 
@@ -138,14 +149,22 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         return round(sqrt(pow((goal_pose.x - now_pose.x), 2) + pow((goal_pose.y - now_pose.y), 2)),4)
 
     def euclidean_degree(self, now_pose, goal_pose):
-        """degree = [0.0001,360]"""
+        """
+        #degree = [0.0001,360]
         degree = round((np.arcsin((goal_pose.y - now_pose.y)/self.euclidean_distance(now_pose, goal_pose))*180/pi), 4)
         if (goal_pose.x - now_pose.x) < 0:
             degree = 180 - degree
         else:
             if degree <= 0:
                 degree += 360
-        return degree
+        """
+        pi_rad = round((np.arcsin((goal_pose.y - now_pose.y)/self.euclidean_distance(now_pose, goal_pose))), 4)
+        if (goal_pose.x - now_pose.x) < 0:
+            if pi_rad >= 0:
+                pi_rad += pi/2 - pi_rad
+            else:
+                pi_rad += -(pi/2) - pi_rad
+        return pi_rad
     
     def calculate_observation(self,data):
         min_range = 0.301
@@ -162,6 +181,7 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
                 else:
                     state_list += [30]
         """
+        """
         for i, item in enumerate(data.ranges):
             if not np.isinf(data.ranges[i]):    
                 if (min_range > data.ranges[i] > 0):
@@ -169,21 +189,38 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
                 state_list += [data.ranges[i]]
             else:
                 state_list += [30]
+        """
+        for i, item in enumerate(data.ranges):
+            if not np.isinf(data.ranges[i]):    
+                if (min_range > data.ranges[i] > 0):
+                    done = True
+                state_list += [round(data.ranges[i]/30, 4)]
+            else:
+                state_list += [1.0000]
                 
-        distance = self.euclidean_distance(self.pose, self.goalpose)
+        distance = min(self.euclidean_distance(self.pose, self.goalpose),30)
         
+        """
         body_to_target_degree = self.euclidean_degree(self.pose, self.goalpose)
         if body_to_target_degree > self.pose.theta:
             direction_to_target_deg = body_to_target_degree - self.pose.theta
         else:
             direction_to_target_deg = 360 - (self.pose.theta - body_to_target_degree)
+        """
+        body_to_target_pi_rad = self.euclidean_degree(self.pose, self.goalpose)
+        direction_to_target_pi_rad = body_to_target_pi_rad - self.pose.theta
+        if (direction_to_target_pi_rad > 1):
+            direction_to_target_pi_rad += -2
+        elif (direction_to_target_pi_rad < -1):
+            direction_to_target_pi_rad += 2
         
         cur_distance = self.euclidean_distance(self.pose, self.goalpose)
         if cur_distance < self.subgoal_as_dist_to_goal :
             self.subgoal_as_dist_to_goal = cur_distance
             self.update_subgoal = True
         
-        state_list += [distance, direction_to_target_deg, self.subgoal_as_dist_to_goal]
+        """state_list += [round(distance/30, 4), round(direction_to_target_deg/360, 4)] #, self.subgoal_as_dist_to_goal]"""
+        state_list += [round(distance/30, 4), round(direction_to_target_pi_rad, 4)] #, self.subgoal_as_dist_to_goal]
         #print body_to_target_degree, direction_to_target_deg
         state_tuple = tuple(state_list)
         return state_tuple, done
@@ -200,10 +237,14 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
 
         vel_cmd = Twist()
         vel_cmd.linear.x = action[0]
-        vel_cmd.linear.y = action[1]
-        vel_cmd.angular.z = action[2]
+        #vel_cmd.linear.y = action[1]
+        #vel_cmd.angular.z = action[2]
+        vel_cmd.angular.z = action[1]
         self.vel_pub.publish(vel_cmd)
 
+        time.sleep(0.01)
+        self.reset_vel()
+        
         data = None
         while data is None:
             try:
@@ -223,11 +264,14 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         prev_distance = self.euclidean_distance(self.beforepose, self.goalpose)
 
         if not done:
+            reward = (prev_distance - cur_distance)*10
+            """
             if self.update_subgoal:
                 reward = 10
                 self.update_subgoal = False
             else:
                 reward = (prev_distance - cur_distance)*10
+            """
         else:
             reward = -10
             """
@@ -253,7 +297,7 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         except (rospy.ServiceException) as e:
             print ("/gazebo/reset_simulation service call failed")
         
-        self.random_obstacle()
+        #self.random_obstacle()
         
         # Unpause simulation to make observation
         rospy.wait_for_service('/gazebo/unpause_physics')
