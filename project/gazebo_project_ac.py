738d0436459d4a33860517fb147d54ffbb6da9a6
hrazan <hafiyanda.razan@gmail.com>
2020-06-01 09:41:35 +0900

    actor_critic v6.2
    all normalized to [0,1]

--- project/gazebo_project_ac.py
+++ project/gazebo_project_ac.py
@@ -132,15 +132,8 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
             self.pose.x = round(data.pose[self.robot_id].position.x, 4)
             self.pose.y = round(data.pose[self.robot_id].position.y, 4)
             quaternion = (round(data.pose[self.robot_id].orientation.x, 4), round(data.pose[self.robot_id].orientation.y, 4), round(data.pose[self.robot_id].orientation.z, 4), round(data.pose[self.robot_id].orientation.w, 4))
-            self.pose.theta = round((tf.transformations.euler_from_quaternion(quaternion)[2]), 4)
-            """
-            yaw = round((tf.transformations.euler_from_quaternion(quaternion)[2]*180/pi), 4)
-            #yaw = [0.0001,360]
-            if yaw <= 0:
-                self.pose.theta = yaw + 360
-            else:
-                self.pose.theta = yaw
-            """
+            yaw = tf.transformations.euler_from_quaternion(quaternion)[2]
+            self.pose.theta = round((yaw + 1)/2, 4)
         except IndexError:
             None
 
@@ -148,23 +141,15 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         """Euclidean distance between current pose and the goal"""
         return round(sqrt(pow((goal_pose.x - now_pose.x), 2) + pow((goal_pose.y - now_pose.y), 2)),4)
 
-    def euclidean_degree(self, now_pose, goal_pose):
-        """
-        #degree = [0.0001,360]
-        degree = round((np.arcsin((goal_pose.y - now_pose.y)/self.euclidean_distance(now_pose, goal_pose))*180/pi), 4)
-        if (goal_pose.x - now_pose.x) < 0:
-            degree = 180 - degree
-        else:
-            if degree <= 0:
-                degree += 360
-        """
-        pi_rad = round((np.arcsin((goal_pose.y - now_pose.y)/self.euclidean_distance(now_pose, goal_pose))), 4)
+    def euclidean_angle(self, now_pose, goal_pose):
+        pi_rad = np.arcsin((goal_pose.y - now_pose.y)/self.euclidean_distance(now_pose, goal_pose))
         if (goal_pose.x - now_pose.x) < 0:
             if pi_rad >= 0:
                 pi_rad += pi/2 - pi_rad
             else:
                 pi_rad += -(pi/2) - pi_rad
-        return pi_rad
+        normalized_angle = round((pi_rad + 1)/2,4)
+        return normalized_angle
     
     def calculate_observation(self,data):
         min_range = 0.301
@@ -181,15 +166,6 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
                 else:
                     state_list += [30]
         """
-        """
-        for i, item in enumerate(data.ranges):
-            if not np.isinf(data.ranges[i]):    
-                if (min_range > data.ranges[i] > 0):
-                    done = True
-                state_list += [data.ranges[i]]
-            else:
-                state_list += [30]
-        """
         for i, item in enumerate(data.ranges):
             if not np.isinf(data.ranges[i]):    
                 if (min_range > data.ranges[i] > 0):
@@ -198,16 +174,8 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
             else:
                 state_list += [1.0000]
                 
-        distance = min(self.euclidean_distance(self.pose, self.goalpose),30)
-        
-        """
-        body_to_target_degree = self.euclidean_degree(self.pose, self.goalpose)
-        if body_to_target_degree > self.pose.theta:
-            direction_to_target_deg = body_to_target_degree - self.pose.theta
-        else:
-            direction_to_target_deg = 360 - (self.pose.theta - body_to_target_degree)
-        """
-        body_to_target_pi_rad = self.euclidean_degree(self.pose, self.goalpose)
+        cur_distance = min(self.euclidean_distance(self.pose, self.goalpose),30)
+        body_to_target_pi_rad = self.euclidean_angle(self.pose, self.goalpose)
         """
         direction_to_target_pi_rad = body_to_target_pi_rad - self.pose.theta
         if (direction_to_target_pi_rad > 1):
@@ -215,16 +183,11 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         elif (direction_to_target_pi_rad < -1):
             direction_to_target_pi_rad += 2
         """
-        cur_distance = self.euclidean_distance(self.pose, self.goalpose)
         if cur_distance < self.subgoal_as_dist_to_goal :
             self.subgoal_as_dist_to_goal = cur_distance
             self.update_subgoal = True
             
-        """state_list += [round(distance/30, 4), round(direction_to_target_deg/360, 4)] #, self.subgoal_as_dist_to_goal]"""
-        
-        #state_list += [round(distance/30, 4), round(direction_to_target_pi_rad, 4)] #, self.subgoal_as_dist_to_goal]
-        
-        state_list += [round(distance/30, 4), round(body_to_target_pi_rad, 4), self.pose.theta]
+        state_list += [round(cur_distance/30, 4), body_to_target_pi_rad, self.pose.theta]
         #print body_to_target_pi_rad, direction_to_target_pi_rad
         state_tuple = tuple(state_list)
         return state_tuple, done
@@ -243,7 +206,6 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         vel_cmd.linear.x = action[0]
         vel_cmd.linear.y = action[1]
         vel_cmd.angular.z = action[2]
-        #vel_cmd.angular.z = action[1]
         self.vel_pub.publish(vel_cmd)
 
         time.sleep(0.01)
@@ -269,23 +231,8 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
 
         if not done:
             reward = (prev_distance - cur_distance)*10
-            """
-            if self.update_subgoal:
-                reward = 10
-                self.update_subgoal = False
-            else:
-                reward = (prev_distance - cur_distance)*10
-            """
         else:
             reward = -10
-            """
-            if  cur_distance < prev_distance and cur_distance < self.subgoal_as_dist_to_goal :
-                reward = 100/cur_distance
-                self.subgoal_as_dist_to_goal = cur_distance
-            else: reward = 0.1
-        else:
-            reward = 0.01
-            """
         
         self.beforepose.x = self.pose.x
         self.beforepose.y = self.pose.y
