7fb5ce0fff8df80d169132d7bc9e5f2d02b794dd
hrazan <hafiyanda.razan@gmail.com>
2020-12-04 21:14:43 +0900

    environment update
    camera setting, max velocity, etc

--- project/gazebo_project_ac.py
+++ project/gazebo_project_ac.py
@@ -38,6 +38,7 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         self.startpose = Pose()
 
         self.goal = False
+        self.goal_radius = 0.500
         self.update_subgoal = False
         self.robot_id = 12
 
@@ -59,8 +60,8 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         """
         
         self.action_space = spaces.Box(
-            low = np.array([-0.3, -0.3, -0.3]),
-            high = np.array([0.3, 0.3, 0.3]),
+            low = np.array([-0.5, -0.5, -0.3]),
+            high = np.array([0.5, 0.5, 0.3]),
             dtype = np.float32
         )
         """
@@ -89,7 +90,7 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
     def random_start(self):
         state_msg = ModelState()
         state_msg.model_name = 'robot'
-        state_msg.pose.position.x = -2.0
+        state_msg.pose.position.x = -6.0
         state_msg.pose.position.y = random.uniform(-1,1)
         state_msg.pose.position.z = 0.1
         quaternion = Quaternion.from_euler(0,0,random.uniform(-pi,pi))
@@ -107,7 +108,7 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
         state_msg = ModelState()
         state_msg.model_name = 'robot'
         quaternion = Quaternion.from_euler(0,0,-pi/4)
-        state_msg.pose.position.x = -2.0
+        state_msg.pose.position.x = -6.0
         state_msg.pose.position.y = 0.0
         state_msg.pose.orientation.z = quaternion[3]
         state_msg.pose.orientation.w = quaternion[0]
@@ -122,14 +123,14 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
     def random_obstacle(self):
         for n in range(0,10):
             
-            max_obs_acc = 0.3 # LEARN: 0.3, TEST: 0.2
+            max_obs_acc = 1
             state_msg = ModelState()
             state_msg.model_name = 'obstacle_'+str(n)
                 
             if n<8:
-                state_msg.pose.position.x = n-1
+                state_msg.pose.position.x = (n+1)*1.5 - 6
             else:
-                state_msg.pose.position.x = 5-n
+                state_msg.pose.position.x = (7-n)*1.5 - 6
             state_msg.pose.position.y = random.uniform(-1,1)
                 
             state_msg.twist.linear.x = 0.0
@@ -253,9 +254,13 @@ class ProjectAcEnv(gazebo_env.GazeboEnv):
 
         state, done = self.calculate_observation(data)
         
-        distance = self.euclidean_distance(self.beforepose, self.goalpose) - self.euclidean_distance(self.pose, self.goalpose)
+        cur_distance = self.euclidean_distance(self.pose, self.goalpose)
+        distance = self.euclidean_distance(self.beforepose, self.goalpose) - cur_distance
         
-        reward = distance + abs(0.5*distance*exp(-abs(self.target_angle)/0.35)) + abs(0.4*distance*(1-exp((0.3-self.lidar_avg)/0.3))) - int(done)
+        if cur_distance <= self.goal_radius: self.goal = True
+        else: self.goal = False
+        
+        reward = (distance + abs(0.5*distance*exp(-abs(self.target_angle)/0.35)) + abs(0.4*distance*(1-exp((0.3-self.lidar_avg)/0.3))))*(1-int(self.goal)) + int(self.goal) - int(done)
         #print("d:"+str(round(prev_distance-cur_distance,4))+"|ang:"+str(round(self.target_angle,3))+"|R:"+str(round(reward,3)))
         
         self.beforepose.x = self.pose.x
